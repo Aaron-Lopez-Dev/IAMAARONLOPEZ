a2f79fe3f0b91e9e78a827ef055d31a629f86c44
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-07-01 21:51:03 -0400

    Responsiveness Updates

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-const textElement = document.getElementById("textChange");
-const fullText = ["DESIGN.","DEVELOP.","HOST."];
-let charIndex = 0;
-const typingSpeed = 200; // milliseconds per character
-fullTextIndex = 0;
+function updateButtons() {
+  const desktopButton = document.getElementById('desktopButton');
+  const mobileButton = document.getElementById('mobileButton');
 
-function typeWriter() {
-    if (charIndex < fullText[fullTextIndex].length) {
-        textElement.textContent += fullText[fullTextIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(typeWriter, typingSpeed);
-    } else if (charIndex == fullText[fullTextIndex].length) {
-
-        textElement.textContent = " ";
-        charIndex = 0;
-        fullTextIndex++;
-
-        if (fullTextIndex >= fullText.length) {
-            fullTextIndex = 0;
-        }
-
-        setTimeout(typeWriter, typingSpeed);
-    }
+  if (window.innerWidth > 900) {
+    desktopButton.style.display = 'inline-block';
+    mobileButton.style.display = 'none';
+  } else {
+    desktopButton.style.display = 'none';
+    mobileButton.style.display = 'inline-block';
+  }
 }
 
-document.addEventListener("DOMContentLoaded", typeWriter())
+window.addEventListener('resize', updateButtons);
+
 

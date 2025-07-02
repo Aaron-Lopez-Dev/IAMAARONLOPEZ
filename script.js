99cf07fae4ec088e72577b536179ecae4e2c3f8d
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-07-01 20:49:00 -0400

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textElement = document.getElementById("textChange");
-const fullText = ["DESIGN","DEVELOP","HOST"];
+const fullText = ["DESIGN.","DEVELOP.","HOST."];
 let charIndex = 0;
-const typingSpeed = 90; // milliseconds per character
+const typingSpeed = 200; // milliseconds per character
 fullTextIndex = 0;
 
 function typeWriter() {
@@ -9,16 +9,19 @@ function typeWriter() {
         textElement.textContent += fullText[fullTextIndex].charAt(charIndex);
         charIndex++;
         setTimeout(typeWriter, typingSpeed);
-    }else if (charIndex == fullText[fullTextIndex].length){
-        textElement.textContent = "";
-        fullTextIndex++;
-        charIndex = 0;
-            typeWriter();
-    }
+    } else if (charIndex == fullText[fullTextIndex].length) {
 
+        textElement.textContent = " ";
+        charIndex = 0;
+        fullTextIndex++;
+
+        if (fullTextIndex >= fullText.length) {
+            fullTextIndex = 0;
+        }
+
+        setTimeout(typeWriter, typingSpeed);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", typeWriter())
-
-//console.log(fullText.length)
 

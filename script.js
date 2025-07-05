ee9b634883db455a624dfe1354c77eb6a831fd11
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-07-05 17:37:05 -0400

    integrated "Component Style Sections"

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Store Buttons 
+const whyButton = document.getElementById('why')
+const serviceButton = document.getElementById('serviceButton')
+const homeButton = document.getElementById('home')
+
+//Store Sections
+const heroSection = document.getElementById('hero');
+const serviceSection = document.getElementById('services')
+const whySection = document.getElementById('whySection')
+
+// On load listeners
+window.addEventListener('load', updateButtons);
+window.addEventListener('load', initialDisplay);
+
+// Button Listener
+whyButton.addEventListener('click', displayWhySection)
+serviceButton.addEventListener('click', displayServiceSection)
+homeButton.addEventListener('click', displayHeroSection)
+
 function updateButtons() {
   const desktopButton = document.getElementById('desktopButton');
   const mobileButton = document.getElementById('mobileButton');
@@ -11,6 +30,46 @@ function updateButtons() {
   }
 }
 
-window.addEventListener('load', updateButtons);
+function initialDisplay() {
+  heroSection.style.opacity = '1'
+  whySection.style.opacity = '0'
+  serviceSection.style.opacity = '0'
+
+      if (window.innerWidth <= 550) {
+    serviceSection.style.height = '80vh'
+  }
+}
+
+function displayHeroSection() {
+  heroSection.style.opacity = '1'
+  whySection.style.opacity = '0'
+  serviceSection.style.opacity = '0'
+
+    if (window.innerWidth <= 550) {
+    serviceSection.style.height = '80vh'
+  }
+
+}
+
+function displayWhySection() {
+  heroSection.style.opacity = '0'
+  whySection.style.opacity = '1'
+  serviceSection.style.opacity = '0'
+
+    if (window.innerWidth <= 550) {
+    serviceSection.style.height = '80vh'
+  }
+
+}
+
+function displayServiceSection() {
+  heroSection.style.opacity = '0'
+  whySection.style.opacity = '0'
+  serviceSection.style.opacity = '1'
+
+  if (window.innerWidth <= 550) {
+    serviceSection.style.height = '170vh'
+  }
+}
 
 
